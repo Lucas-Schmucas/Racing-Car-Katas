34344c56d3fddff8fa99be7ebac2e565a2f26798
lucas-schmucas <l.breiner@clipmyhorse.tv>
2025-01-17 16:15:46 +0100

    Solve HtmlTextConverter Refactoring

--- php/tests/TextConverter/HtmlTextConverterTest.php
+++ php/tests/TextConverter/HtmlTextConverterTest.php
@@ -9,9 +9,37 @@ use RacingCar\TextConverter\HtmlTextConverter;
 
 class HtmlTextConverterTest extends TestCase
 {
-    public function testFoo(): void
+    public function testRemoveEmptySpaces(): void
     {
         $converter = new HtmlTextConverter('foo');
-        $this->assertSame('fixme', $converter->getFileName());
+        $result = $converter->removeEmptySpace('3_spaces_on_the_end   ');
+        $this->assertStringContainsString('3_spaces_on_the_end', $result);
+        $this->assertStringNotContainsString(' ', $result);
+    }
+
+    public function testConvertSpecialChars() : void
+    {
+        $converter = new HtmlTextConverter('foo');
+        $result = $converter->convertSpecialChars('<p>foo</p>');
+        $this->assertStringContainsString('&lt;p&gt;foo&lt;/p&gt;', $result);
+    }
+
+    public function testSetBreakAtEndOfLine() : void
+    {
+        $converter = new HtmlTextConverter('foo');
+        $result = $converter->setBreakAtEndOfLine('this is a line');
+        $this->assertStringEndsWith('<br>', $result);
+    }
+
+    public function testConvertToHtml() : void
+    {
+        $fileName = 'tests/TextConverter/convert_me_sensei';
+        $converter = new HtmlTextConverter($fileName);
+        $this->assertSame($converter->getFileName(), $fileName);
+
+
+        $result = $converter->convertToHtml();
+
+        $this->assertSame($result, '<br>two breaks, one special char &quot; and two spaces at the end<br>');
     }
 }
